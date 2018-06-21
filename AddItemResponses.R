091a88931a074386437cc8f225a3b1a56076645f
debarros <pj.debarros@gmail.com>
2018-06-21 10:27:47 -0400

    Added print statements.  Removed some unnecessary stuff from AddItemResponses.R

--- AddItemResponses.R
+++ AddItemResponses.R
@@ -4,14 +4,22 @@
 
 AddItemResponses = function(path){
   
+  print("1")
+  
   x = read.xlsx(path)
+  
+  print("2")
   
   # fix date variables
   x$Birthdate = xlDate(x$Birthdate)
   x$Test.Date = xlDate(x$Test.Date)
   
+  print("3")
+  
   # set the number of MC questions
   MCQs = 30 
+  
+  print("4")
   
   # get the CRQ scores
   Part2 = substr(x$Constructed.Response.Array,1,3)
@@ -22,13 +30,17 @@ AddItemResponses = function(path){
   z = as.data.frame(z, stringsAsFactors = F)
   rownames(z) = NULL
   z = z[,3*(1:12)]
-  colnames(z) = c("1", "2", "3A", "3B", "4", "5", "6A", "6B", "7", "8", "9A", "9B")
+  colnames(z) = c("D1", "D2", "D3A", "D3B", "D4", "D5", "D6A", "D6B", "D7", "D8", "D9A", "D9B")
   CRQs = cbind.data.frame(Part2, z, Part3B)
+  
+  print("5")
   
   #create variables for all the MC questions
   for (i in 1:MCQs){
     assign(paste0("Q",i), gsub("D", 4, gsub("C", 3,gsub("B",2,gsub("A",1,substr(x$Multiple.Choice.Response.Array, i, i))))))
   }
+  
+  print("6")
   
   # Create the MC answer key
   answerKey = c("A", "C", "B", "A", "C", "B", "D", "B", "B", "D", "A", "C", "A", "A", "B", "D", "A", "A", "C", "A", "D", "D", "B", "D", "A", "C", "D", "C", "C", "D")
@@ -37,26 +49,30 @@ AddItemResponses = function(path){
   answerKey = gsub("C", "3", answerKey)
   answerKey = gsub("D", "4", answerKey)
   
+  print("7")
+  
   # create a single variable for the student name
   LastFirst = paste0(x$Student.Last.Name, ", ", x$Student.First.Name)
   hasMidInit = !is.na(x$Student.Middle.Initial)
   LastFirst[hasMidInit] = paste0(x$Student.Last.Name[hasMidInit], ", ", x$Student.First.Name[hasMidInit], " ", x$Student.Middle.Initial[hasMidInit])
   
-  
+  print("8")
   
   
   # combine the new variables with the existing data
   z = do.call(cbind, mget(c("LastFirst", "x", paste0("Q", 1:MCQs))))
   z = cbind.data.frame(z, CRQs)
   
-  itemsToCheck = setdiff(2:37,1+3*1:12)
-  unique(unlist(CRQs[,itemsToCheck]))
+  print("9")
   
+  print("10")
   
   #fix the variables names
   Varnames = colnames(z)
   Varnames = gsub("x.","", Varnames)
   colnames(z) = Varnames
+  
+  print("11")
   
   return(z)
   
--- functions.R
+++ functions.R
@@ -1,5 +1,15 @@
 # functions.R
 
+print("functions 1")
+
 library(openxlsx)
+
+print("functions 2")
+
 xlDate = function(x){  as.Date(x, origin = "1899-12-30") }
+
+print("functions 3")
+
 source("AddItemResponses.R")
+
+print("functions 4")
